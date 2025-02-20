4166f041dc38571ef7609b6646213e0e63ed0516
Waldemar Tomme <waldemar.tomme@BlueHands.de>
2025-02-20 09:27:55 +0100

    Implement simple shim with fixed configuration

--- WSLr.Shim/Program.cs
+++ WSLr.Shim/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CliWrap;
+
+var debugEnabled = true;
+
+DebugWriteLine("<init>");
+var wslBinary = "ls";
+DebugWriteLine("Binary: {0}", wslBinary);
+DebugWriteLine("Arguments: {0}", string.Join(" ", args));
+
+
+DebugWriteLine("<exec>");
+var result = await Cli.Wrap("wsl")
+    .WithArguments(builder => builder
+        .Add(wslBinary)
+        .Add(args))
+    .WithStandardInputPipe(PipeSource.FromStream(Console.OpenStandardInput()))
+    .WithStandardOutputPipe(PipeTarget.ToStream(Console.OpenStandardOutput()))
+    .WithStandardErrorPipe(PipeTarget.ToStream(Console.OpenStandardError()))
+    .WithValidation(CommandResultValidation.None)
+    .ExecuteAsync();
+
+DebugWriteLine("<exit>");
+DebugWriteLine("Exit Code: {0}", result.ExitCode);
+
+return result.ExitCode;
+
+void DebugWriteLine(string format, params object[] args)
+{
+    if (!debugEnabled)
+        return;
+
+    Console.Error.WriteLine($"[WSLr] {string.Format(format, args)}");
+}
